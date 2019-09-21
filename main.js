5991debb83f67e7325b494b99cffffe798496923
Yang Sun <ysun62@ucsc.edu>
2019-09-21 16:55:57 -0700

    added page highlighting

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
-//Smooth Scroll
-const scroll = new SmoothScroll('a[href*="#"]', {speed: 500});
-
-
-
 const about = document.querySelector('#about');
 const projects = document.querySelector('#projects');
 const contact = document.querySelector('#contact');
 const navItems = document.querySelectorAll('ul li');
 const whole_page = document.querySelector('.container');
-
 const abouth1 = document.querySelector('#about h1');
 const abouth1Top = abouth1.offsetTop;
+
+
+//Smooth Scroll
+const scroll = new SmoothScroll('a[href*="#"]', {speed: 500});
+
 
 //Nav Animation
 const navAnimation = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('ul');
-
 
     burger.addEventListener('click', () => {
         //Toggle Nav
@@ -37,72 +35,82 @@ const navAnimation = () => {
 }
 
 navAnimation();
-
-//Page Animation
-const pageAnimation = () => {
-    navItems.forEach((item) => {
-        item.addEventListener('click', () => {
-            // console.log(page.textContent);
-            // if(item.textContent === 'About') {
-                // about.classList.add('about-landing');
-            // } else if(item.textContent === 'Projects') {
-            //     projects.classList.add('projects-landing');
-            // }
-            
-        });
-    });
-        
-}
-
-pageAnimation();
+  
 
 //Sticky Nav + Some Animation
 const nav = document.querySelector('nav');
 const navTop = nav.offsetTop;
 
 const handleScroll = () => {
-    // if(window.scrollY >= 50vh) {
-    //     console.log('more than 50');
-    // }
-    console.log(window.scrollY, navTop);
+    // console.log(window.scrollY, pageYOffset);
     if(window.scrollY >= navTop) {
         document.body.style.paddingTop = nav.offsetHeight + 'px';
         nav.classList.add('fixed-nav');
-        console.log('fixed');
     } else {
         document.body.style.paddingTop = 0;
         nav.classList.remove('fixed-nav');
     }
 }
 
-
-
 const scrollAnimation = () => {
-    console.log(window.innerWidth);
-    if(window.scrollY >= 460 && window.scrollY <= 1180) {
+    if(window.scrollY/window.innerHeight >= 0.3 && window.scrollY/window.innerHeight <= 1.13) {
         about.children[0].classList.add('animation-slide-in-right');
         about.children[0].classList.add('about-landing');
     }
-    if(window.scrollY >= 750 && window.scrollY <= 1660) {
+    if(window.scrollY/window.innerHeight >= 0.69 && window.scrollY/window.innerHeight <= 1.465) {
         about.children[1].classList.add('animation-slide-in');
     }
-    if(window.scrollY >= 1523 && window.scrollY <= 2246) {
+    if(window.scrollY/window.innerHeight >= 1.3 && window.scrollY/window.innerHeight <= 2.15) {
         projects.children[0].classList.add('animation-slide-in-right');
         projects.children[0].classList.add('projects-landing');
     }
-    if(window.scrollY >= 1700 && window.scrollY <= 2700) {
+    if(window.scrollY/window.innerHeight >= 1.53 && window.scrollY/window.innerHeight <= 2.5) {
         projects.children[1].children[0].classList.add('animation-slide-in');
     }
-    // if(window.scrollY >= 1900 && window.scrollY <= 2700) {
+    if(window.scrollY/window.innerHeight >= 2.15 && window.scrollY/window.innerHeight <= 2.8) {
         projects.children[1].children[1].classList.add('animation-slide-in');
-    // }
-    // if(window.scrollY >= 2586) {
+    }
+    if(window.scrollY/window.innerHeight >= 2.46) {
         contact.children[0].classList.add('animation-slide-in-right');
         contact.children[0].classList.add('projects-landing');
-    // }
+    }
 }
+
+const pageHighlight = () => {
+    const sections = document.querySelectorAll('ul li a');
+    let scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+    
+    for(let i = 0; i < sections.length-1; i++) {
+        let current_page = sections[i]; 
+        let value = current_page.getAttribute('href');
+        let page = document.querySelector(value);
+        if(page.offsetTop <= scrollPos && (page.offsetTop + page.offsetHeight > scrollPos)) {
+            document.querySelector('ul li a').classList.remove('current-page');
+            current_page.classList.add('current-page');
+        } else {
+            current_page.classList.remove('current-page');
+        }
+    }
+    
+};
 
 window.addEventListener('scroll', () => {
     handleScroll(); 
     scrollAnimation();
+    pageHighlight();
 });
+
+//Highlight Current Page
+// const items = document.querySelectorAll('ul li a');
+// const pageHighlight = () => {
+//     items.forEach((item) => {
+//         item.addEventListener('click', () => {
+//             items.forEach((i) => {
+//                 i.classList.remove('current-page');
+//             });
+//             item.classList.add('current-page');
+//         });
+//     });
+// }
+
+// pageHighlight();
