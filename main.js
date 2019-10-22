e4b50213a6a312b1fd9f5cc122ba5399bf82a4fb
Yang Sun <ysun62@ucsc.edu>
2019-10-21 23:17:13 -0700

    fixed nav bugs

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 // Smooth Scroll
-const scroll = new SmoothScroll('a[href*="#"]', {speed: 500});
-
+const scroll = new SmoothScroll('a[href*="#"]', { speed: 500 });
 
 // Sticky Nav
-const nav = document.querySelector('nav');
+const nav = document.querySelector("nav");
 // const navTop = nav.offsetTop;
 
 // const handleScroll = () => {
@@ -16,87 +15,89 @@ const nav = document.querySelector('nav');
 //     }
 // }
 
-window.addEventListener('scroll', () => {
-    // handleScroll();
-    pageHighlight();
+window.addEventListener("scroll", () => {
+  // handleScroll();
+  pageHighlight();
 });
 
 // Handling Click
-const burger = document.querySelector('.burger');
-const ul = document.querySelector('#nav-ul');
-const ulItems = document.querySelectorAll('#nav-ul li');
-const navLinks = document.querySelectorAll('#nav-ul li a');
+const burger = document.querySelector(".burger");
+const ul = document.querySelector("#nav-ul");
+const ulItems = document.querySelectorAll("#nav-ul li");
+const navLinks = document.querySelectorAll("#nav-ul li a");
 const handleClick = () => {
-    // Remove Burger Animation
-    burger.classList.remove('toggleBurger');
+  // Remove Burger Animation
+  burger.classList.remove("toggleBurger");
 
-    // Change Border-Bottom
-    nav.style.borderBottom = '3px solid #51BE95';
-    ul.style.borderBottom = 'none';
+  // Change Border-Bottom
+  nav.style.borderBottom = "3px solid #51BE95";
+  ul.style.borderBottom = "none";
 
-    // Nav Closing Animation
-    ul.style.animation = 'navFadeAway-portrait 0.5s forwards ease-in-out';
+  // Nav Closing Animation
+  ul.style.animation = "navFadeAway-portrait 0.5s forwards ease-in-out";
 
-    // Makes Sure Nav Animation Ends First
-    setTimeout(() => {
-        ul.classList.add('hide-on-mobile');
-    }, 500);
+  // Makes Sure Nav Animation Ends First
+  setTimeout(() => {
+    ul.classList.add("hide-on-mobile");
+  }, 500);
 
-    // NavLink Fade Away Animation
-    ulItems.forEach(item => {
-        item.style.animation = `navLinkFadeOpposite 0.5s ease-in-out backwards`
-    });
-}
+  // NavLink Fade Away Animation
+  ulItems.forEach(item => {
+    item.style.animation = `navLinkFadeOpposite 0.5s ease-in-out backwards`;
+  });
+};
 
 // Nav Animation
 const navAnimation = () => {
-    burger.addEventListener('click', () => {
-        if(ul.classList.contains('hide-on-mobile')) {
-            // Show Nav
-            ul.classList.remove('hide-on-mobile');
+  burger.addEventListener("click", () => {
+    if (ul.classList.contains("hide-on-mobile")) {
+      // Show Nav
+      ul.classList.remove("hide-on-mobile");
 
-            ul.style.animation = 'navFade-portrait 0.5s forwards ease-in-out';
-            
-            // Change Border-Bottom
-            nav.style.borderBottom = 'none';
-            ul.style.borderBottom = '3px solid #51BE95';
+      ul.style.animation = "navFade-portrait 0.5s forwards ease-in-out";
 
-            // Animate NavItems
-            ulItems.forEach((item, index) => {
-                item.style.animation = `navLinkFade 0.5s ease-in-out forwards ${index / 10}s`
-            });
+      // Change Border-Bottom
+      nav.style.borderBottom = "none";
+      ul.style.borderBottom = "3px solid #51BE95";
 
-            // Animate Burger
-            burger.classList.add('toggleBurger');
-        } else {
-            handleClick();
-        }
+      // Animate NavItems
+      ulItems.forEach((item, index) => {
+        item.style.animation = `navLinkFade 0.5s ease-in-out forwards ${index /
+          10}s`;
+      });
 
-        // Close Nav After Click Link
-        navLinks.forEach(link => {
-            link.addEventListener('click', () => {
-                handleClick();
-            });
-        });
+      // Animate Burger
+      burger.classList.add("toggleBurger");
+    } else {
+      handleClick();
+    }
+
+    // Close Nav After Click Link
+    navLinks.forEach(link => {
+      link.addEventListener("click", () => {
+        handleClick();
+      });
     });
-
-}
+  });
+};
 
 navAnimation();
 
 const pageHighlight = () => {
-    let scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
-    
-    for(let i = 0; i < navLinks.length-1; i++) {
-        let current_page = navLinks[i]; 
-        let value = current_page.getAttribute('href');
-        let page = document.querySelector(value);
-        if(page.offsetTop <= scrollPos && (page.offsetTop + page.offsetHeight > scrollPos)) {
-            document.querySelector('ul li a').classList.remove('current-page');
-            current_page.classList.add('current-page');
-        } else {
-            current_page.classList.remove('current-page');
-        }
+  let scrollPos = window.pageYOffset || document.documentElement.scrollTop;
+
+  for (let i = 0; i < navLinks.length - 1; i++) {
+    let current_page = navLinks[i];
+    let value = current_page.getAttribute("href");
+    let page = document.querySelector(value);
+    if (
+      page.offsetTop <= scrollPos &&
+      page.offsetTop + page.clientHeight - 10 > scrollPos
+    ) {
+      document.querySelector("ul li a").classList.remove("current-page");
+      current_page.classList.add("current-page");
+    } else {
+      current_page.classList.remove("current-page");
     }
-    
+  }
 };
